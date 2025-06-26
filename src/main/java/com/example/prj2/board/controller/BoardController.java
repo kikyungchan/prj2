5435b44c458d5978a16916def3664704d1342742
kikyungchan <kikyungchan@gmail.com>
2025-06-26 10:27:00 +0900

    로그인 여부에 따라 글 작성화면 이동가능

--- src/main/java/com/example/prj2/board/controller/BoardController.java
+++ src/main/java/com/example/prj2/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.prj2.board.controller;
 import com.example.prj2.board.dto.BoardDto;
 import com.example.prj2.board.dto.BoardForm;
 import com.example.prj2.board.service.BoardService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,9 +22,19 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("write")
-    public String writeForm() {
+    public String writeForm(HttpSession session, RedirectAttributes rttr) {
+        Object user = session.getAttribute("loggedInUser");
+        if (user == null) {
+            rttr.addFlashAttribute("alert",
+                    Map.of("code", "warning",
+                            "message", "로그인 후 글을 작성 해주세요."));
+            return "redirect:/member/login";
+        } else {
 
-        return "board/write";
+            return "board/write";
+        }
+
+
     }
 
     @PostMapping("write")
